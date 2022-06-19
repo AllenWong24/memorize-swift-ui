35b41dd7606a4066e3cb7f0034d83d61738726ad
AllenWong <allenwong2407@outlook.com>
2022-06-19 16:56:01 +0800

    Initial Commit

---
+++ memorize-swift-ui/memorize_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorize_swift_uiApp.swift
+//  memorize-swift-ui
+//
+//  Created by 黄家健 on 2022/6/19.
+//
+
+import SwiftUI
+
+@main
+struct memorize_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
